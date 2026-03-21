451a977bf8e7ee063fa0e53f82308b49c1a62b3b
aan4ndd <anandpryo@gmail.com>
2026-03-22 01:57:42 +0530

    Login Page

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="header">
+        <div class="logo">Bargain</div>
+
+    </div>
+    <div class="signupForm">
+        <p>username</p>
+        <form action=""></form>
+    </div>
+</body>
+</html>
